ef7f33bd92c0b29c04fbc721590d1882abc04c5c
kelvinchanwh <35751887+kelvinchanwh@users.noreply.github.com>
2021-03-06 16:59:37 +0800

    Added BayesianOptimization

---
+++ randGridSearch.py
@@ -0,0 +1,63 @@
+import logging
+import numpy as np
+import random
+import torch
+import util.dataToGoogle as Google
+import util.tool
+import pandas as pd
+from util.configue import Configure
+
+from bayes_opt import BayesianOptimization
+
+# Bounded region of parameter space
+pbounds = {'x': (0, 1.0), 'y': (0, 1.0)}
+
+def start(x, y):
+
+    logging.basicConfig(level = logging.INFO)
+
+    args = Configure.Get()
+
+    np.random.seed(args.train.seed)
+    random.seed(args.train.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.train.seed)
+        torch.cuda.manual_seed(args.train.seed)
+
+    torch.manual_seed(args.train.seed)
+    torch.random.manual_seed(args.train.seed)
+
+    Model, DatasetTool = util.tool.load_module(args.model.name, args.dataset.tool)
+
+    #Insert variables to Grid Search
+    args.train.ratio = x
+    args.train.cross = y
+
+    inputs = DatasetTool.get(args)
+
+    model = Model(args, DatasetTool, inputs)
+    if args.train.gpu:
+        model.cuda()
+
+    model.start(inputs)
+
+    return df[x*10][y*10]
+
+# if __name__ == "__main__":
+#     start()
+
+optimizer = BayesianOptimization(
+    f=start,
+    pbounds=pbounds,
+    verbose=2, # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+    random_state=1,
+)
+    
+optimizer.maximize(
+    init_points=2,
+    n_iter=3,
+)
+
+print(optimizer.max)
+for i, res in enumerate(optimizer.res):
+    print("Iteration {}: \n\t{}".format(i, res))
